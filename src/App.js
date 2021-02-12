d7d07e0817bf91afdb1806570602703475747d5e
Keyur Vadodariya <vadodariyakeyur321@gmail.com>
2021-02-12 19:39:16 +0530

    Ingredients addition and removal working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Layout>
+      <BurgerBuilder />
+    </Layout>
   );
 }
 
